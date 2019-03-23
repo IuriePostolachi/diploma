b780d8aa50588817b025aa352844e8baa52dfa51
IuriePostolachi <martolod14@gmail.com>
2019-03-23 14:39:33 +0200

    верстка подвала

--- dist/js/slide.js
+++ dist/js/slide.js
@@ -1,24 +1,14 @@
-// $(document).ready(function () {
-//   $('.slider').slick({
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-//     prevArrow: $('.arrows__left'),
-//     nextArrow: $('.arrows__right'),
-//     responsive: [
-//       {
-//         breakpoint: 1200,
-//         settings: {
-//           slidesToShow: 2,
-//           slidesToScroll: 1,
-//         }
-//       },
-//       {
-//         breakpoint: 770,
-//         settings: {
-//           slidesToShow: 1,
-//           slidesToScroll: 1
-//         }
-//       }
-//     ]
-//   });
-// });
+$(document).ready(function() {
+    $('.portfolio-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: $('.portfolio-arrows__left'),
+        nextArrow: $('.portfolio-arrows__right')
+    })
+    $('.clients-slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        prevArrow: $('.clients-arrows__left'),
+        nextArrow: $('.clients-arrows__right')
+    })
+})
